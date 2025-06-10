dd8a3c33bf63d5203455674ffeced2b67bfadd6b
Vinay kandregula <vinaykandregula.180@gmail.com>
2025-06-10 09:27:12 +0530

    Update 206_Reverse_Linked_List.py

--- LinkedList/206_Reverse_Linked_List.py
+++ LinkedList/206_Reverse_Linked_List.py
@@ -1,19 +1,31 @@
-# LeetCode 206: Reverse Linked List
-# https://leetcode.com/problems/reverse-linked-list/
+# LeetCode 206. Reverse Linked List
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/reverse-linked-list/
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+"""
+🧠 Problem:
+Reverse a singly linked list.
+
+Example:
+Input: [1,2,3,4,5]
+Output: [5,4,3,2,1]
+"""
+
+# ✅ Final Working Code
+# Time: O(N), Space: O(1)
 
 def reverseList(head):
     prev = None
-    curr = head
-
-    while curr:
-        next_node = curr.next
-        curr.next = prev
-        prev = curr
-        curr = next_node
-
+    while head:
+        next_node = head.next     # save next
+        head.next = prev          # reverse pointer
+        prev = head               # move prev forward
+        head = next_node          # move head forward
     return prev
+
+"""
+📝 Notes:
+- Standard in-place reversal using 3 pointers.
+- Important: store head.next before modifying.
+"""
+
