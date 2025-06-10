3ea797ce7db2d1c10d903163bd61a380af41c198
Vinay kandregula <vinaykandregula.180@gmail.com>
2025-06-10 10:59:18 +0530

    Update 876_Middle_of_Linked_List.py

--- LinkedList/876_Middle_of_Linked_List.py
+++ LinkedList/876_Middle_of_Linked_List.py
@@ -18,6 +18,31 @@ Output: Node with value 4
 # ✅ Approach 1: Count Nodes and Return Middle
 # Time: O(N), Space: O(1)
 
+def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+    temp = head
+    count = 0
+    while head:
+        head = head.next
+        count += 1 #counting starts after 1st head
+
+    val = count//2 # This gives 0-based index directly
+
+    if count == val == 1:
+        return head
+
+    new_count = 0
+    while temp:
+        if new_count == val: #checks with before count then we reach node matchs val.
+            return temp
+        new_count += 1
+        temp = temp.next
+    # if we check condition first,then it checks before new_count and gives from current temp
+    # when 
+    # considers new_count before current temp.
+
+
+#better than previous approach
 def middleNode(head):
     temp = head
     count = 0
@@ -27,9 +52,11 @@ def middleNode(head):
 
     mid = count // 2
     temp = head
-    for _ in range(mid):
+    # for _ in range(mid):
+    #     temp = temp.next
+    while mid:
         temp = temp.next
-
+        mid -= 1
     return temp
 
 
