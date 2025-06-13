34897ccacf2b7d3cd730f762cd809e61872c4c1a
Vinay kandregula <vinaykandregula.180@gmail.com>
2025-06-13 08:29:36 +0530

    Update Reorder_List.py

--- Linked_List/Reorder_List.py
+++ Linked_List/Reorder_List.py
@@ -33,185 +33,193 @@ just like connecting two linkedlist. [1 -> 5 -> 2 -> 4 -> 3 -> None]
                   https://leetcode.com/problems/reverse-linked-list/         --> to reverse list
 
 
-    --> I originally thought breaking first half isn’t needed, but it actually prevents cycles and pointer bugs when merging.
-    
-    --> Even though the reversed list ends in None, the first half still points into the old second half unless you break it.
+STEPS : 
 
-    So yes — you must break first half manually using prev.next = None
-    It’s not for the second half — it's for cutting the old connection from first half to second half.
+step1 : Find mid and separate two halfs.
 
-
-
-Now separate list into two halves.
-
-       --> disconnect at mid. for that find length and take length//2
+        disconnect at mid. for that find length and take length//2
 
             length = 0
             while head:
-               head = head.next
-               length += 1
+                head = head.next
+                length += 1
 
-mistake 1: moved head upto length which means head is None now. so always store orginal head in other variables.('ptr' as of now)
+            mid = length//2
 
-Fix --->
-            length = 0
-            ptr = head
-            while ptr:
-               ptr = ptr.next
-               length += 1
-
-         Find mid now:    mid = length//2
+        ❌ mistake Here: i moved head upto length which means head is None now. so always store orginal head in other variables.('ptr' as of now)
+    
+        ✅ Fix --->    
+                    ```python
+                    length = 0
+                    ptr = head
+                    while ptr:
+                       ptr = ptr.next
+                       length += 1
+    
+                 Find mid now:    mid = length//2
             
-    --> loop head upto mid and at last in before mid position connect to None.
 
-            temp = curr = head
+    --> To split first and second halfs:
+                
+            temp = curr = head  # always store head in temporaray variables.
             prev = None
             for _ in range(mid):  # Assume we got mid=2  move loop upto mid+1 (to reach upto Node 3)
                prev = temp     #always stays before mid
                temp = temp.next
+               
 
-                    prev  mid
-                      ↓    ↓          
-         list :  1 -> 2 -> 3 -> 4 -> 5 -> None
-         index:  0    1    2    3    4
+                                        prev  mid
+                                          ↓    ↓          
+                             list :  1 -> 2 -> 3 -> 4 -> 5 -> None
+                             index:  0    1    2    3    4
+
 
           * store temp in prev (before mid node. mid is first node of second half)
           * prev is last node of first half.
 
-    *--> I originally thought breaking first half isn’t needed, but it will create cycle during merge and pointer bugs when merging.
+      ❌    I originally thought breaking first half isn’t needed, but it will create cycle during merge and pointer bugs when merging.
 
         But we need to disconnect at prev. this way after loop ends (loop going to mid+1)
-        --> prev.next = None. (remember to initialize prev = None before loop starts to avoid errors)
+        
+            --> prev.next = None. (remember to initialize prev = None before loop starts to avoid errors)
 
-        First Half:
-                list:  1 -> 2 -> None      
-                index: 0    1        
-            
-            At '2'(prev) first half ends and At '3'(mid position) second half start.
-            
-        Second Half:
-                list: 3 -> 4 -> 5 -> None
+                First Half:
+                        list:  1 -> 2 -> None      
+                        index: 0    1        
+                    
+                    At '2'(prev) first half ends and At '3'(mid position) second half start.
+                    
+                Second Half:
+                        list: 3 -> 4 -> 5 -> None
 
         In both odd and even length lists, we treat mid as the boundary where we split the list into:
                         First Half → Nodes before mid
                         Second Half → Starts at mid
 
        
-    mid = length//2 --> gives result when even length and/or with odd length. needs of (mid+1) which may critical to think later.
+        mid = length//2 --> gives result when even length and/or with odd length. needs of (mid+1) which may critical to think later.
 
       ****   so avoid this for better cleaner code.
 
          
-      use slow/fast pointer ---> gives middle exactly for both even and odd.
-
-          slow = fast = head
-          prev = None
-          while fast and fast.next:
-             prev = slow  
-             slow = slow.next
-             fast = fast.next.next
-          prev.next = None   
-          
-                prev  slow
-                 |    |  
-            1 -> 2 -> 3 -> 4 -> 5 -> None
-            
-            
-          # Now `slow` is at start of 2nd half
-          # `prev` is at end of 1st half
-         
-          slow is at mid position even / odd exactly (no need of mid+1 explicity) 
-          temp = slow   #store slow in temp
-          
-
-mistake : If your input linked list has less than 2 nodes (especially only 1 node), the while fast and fast.next loop.
-          will not run even once, so prev stays None. Then trying prev.next causes the error.
-
-Fix :  Add a guard condition before prev.next = None
-                if prev:
-                    prev.next = None   # only disconnect if prev exists
-                    
-    means initially prev is at None. 
-    so to make prev to move forward we need atleast >2 nodes in list.else slow and fast pointer doesnot move.prev still at None(which raises error)
-
-    more better way to for early return.
-    add at beginning of code.
+          use slow/fast pointer ---> gives middle exactly for both even and odd.
     
-            if not head or not head.next:
-                return
+              ```python
+              slow = fast = head
+              prev = None
+              while fast and fast.next:
+                 prev = slow  
+                 slow = slow.next
+                 fast = fast.next.next
+              prev.next = None   
+              
+                    prev  slow
+                     |    |  
+                1 -> 2 -> 3 -> 4 -> 5 -> None
+                
+                
+              # Now `slow` is at start of 2nd half
+              # `prev` is at end of 1st half
+             
+              slow is at mid position even / odd exactly (no need of mid+1 explicity) 
+              temp = slow   #store slow in temp
+          
+
+        ❌ mistake : If your input linked list has less than 2 nodes (especially only 1 node), the while fast and fast.next loop.
+                  will not run even once, so prev stays None. Then trying prev.next causes the error.
+        
+        ✅ Fix :  Add a guard condition before prev.next = None
+
+                        ```python
+                        if prev:
+                            prev.next = None   # only disconnect if prev exists
+                        ```   
+            means initially prev is at None. 
+            so to make prev to move forward we need atleast >2 nodes in list.else slow and fast pointer doesnot move.prev still at None(which raises error)
+        
+            more better way to for early return.
+            add at beginning of code.
+
+            ### 🛡️ Edge Case Handling
+
+                    If list has 0 or 1 node, don't do anything:
+                    
+                    ```python
+                    if not head or not head.next:
+                        return
+                    ```
 
 
-   ---> Reverse 2nd half
-
+ step2: Reverse 2nd half
+   
+           ```python
            rev_temp = None
-           while temp:
-               next_node = temp.next
-               temp.next = rev_temp
-               rev_temp = temp
-               temp = next_node
+           while slow:
+               next_node = slow.next
+               slow.next = rev_temp
+               rev_temp = slow
+               slow = next_node
 
         second half : 5 -> 4 -> 3 -> None
             
             
       
-   ---> now merge two halfs.                                   first
-                                                                ↓ 
-           first = curr                                         1 -> 2 -> None
-           second = rev_temp                                    5 -> 4 -> 3 -> None
-           while first:                                         ↑
-               tmp1 = first.next                              second
-               tmp2 = second.next
+step3:  merging two halfs
 
-               first.next = second
-               second.next = tmp1                                 tmp1
-                                                                    ↓ 
-               first = tmp1                              1 -> 2 -> None
-               second = tmp2                             5 -> 4 -> 3 -> None
-                                                              ↑
-                                                            tmp2
-
-        here i got stuck because i only check condition for when second ends. but what if first reachs to none before second reach.
-        it raised error.
-        
-    Fix:  while first and second
-
-    Another error i got stuck:
+                                                                  first
+               ```python                                            ↓ 
+               first = head                                         1 -> 2 -> None
+               second = rev_temp                                    5 -> 4 -> 3 -> None
+               while first:                                         ↑
+                   tmp1 = first.next                              second
+                   tmp2 = second.next
     
-        after first(1st half node) connected to second(2nd half node). and second of 2nd half node to next node of 1st half it has None. 
-        which raised error.
-            
-    fix ---->
+                   first.next = second
+                   second.next = tmp1                                 tmp1
+                                                                        ↓ 
+                   first = tmp1                              1 -> 2 -> None
+                   second = tmp2                             5 -> 4 -> 3 -> None
+                                                                  ↑
+                                                                tmp2
+    
+        ❌ Mistake: here i got stuck because i only check condition for when second ends. but what if first reachs to none before second reach.
+            it raised error.
+        
+        ✅ Fix:  while first and second
 
-        only connected to first half node when it exists else connected to same second half node
-         
-            first.next = second
-               if tmp1 is None:
-                   second.next = tmp2
-               else:
-                   second.next = tmp1 
+
+        ❌ Mistake: Another error i got stuck:
+        
+            after first(1st half node) connected to second(2nd half node). and second of 2nd half node to next node of 1st half it has None. 
+            which raised error.
+                
+        ✅fix ---->
+    
+            only connected to first half node when it exists else connected to same second half node
+    
+                ```python
+                first.next = second
+                   if tmp1 is None:
+                       second.next = tmp2
+                   else:
+                       second.next = tmp1 
  
 
    ------> code worked <-----
 
-==================================================================
+===============================================================================
+
 🚧 Where I Went Wrong:
 
-❌ Mistake 1: Used `length//2` and looped up to mid — worked for some cases, failed in others.  
-✅ Fix 1   : Used slow/fast pointer approach to accurately find mid for both odd/even lists.
+    | Mistake                                                | Fix                                             |
+    | ------------------------------------------------------ | ----------------------------------------------- |
+    | ❌ Used `length // 2` to find mid (buggy in edge cases) | ✅ Used slow/fast pointer                        |
+    | ❌ Didn't disconnect first half → caused cycles         | ✅ `prev.next = None` to break                   |
+    | ❌ Merge logic broke for odd-length list                | ✅ Check both `first` and `second` while merging |
+    | ❌ Forgot edge case: 0 or 1 node                        | ✅ Early return guard clause                     |
+    | ❌ Assumed both lists are same length                   | ✅ Careful handling inside `while` merge loop    |
 
-❌ Mistake 2: Didn't disconnect the first half from the second → led to cycles while merging.  
-✅ Fix 2   : Used `prev.next = None` to properly break the list at mid.
-
-❌ Mistake 3: Merge logic incorrect for odd-length list.  
-✅ Fix 3   : Carefully merged nodes and broke when necessary to avoid dangling pointers.
-
-❌ Mistake 4: checked condition only for second half to finish
-✅ Fix 4   : need loop to check both halfs.
-
-❌ Mistake 5: after first(1st half node) connected to second(2nd half node). and second of 2nd half node to next node of 1st half it has None. 
-              which raised error.
-✅ Fix 5   :  only connected to first half node when it exists else connected to same second half node
-==================================================================
+===============================================================================
 """
 
 # Definition for singly-linked list.
@@ -239,16 +247,16 @@ def reorderList(head):
     # Step 2: Reverse second half
     #reverse temp
       rev_temp = None
-      while temp:
-            next_node = temp.next
-            temp.next = rev_temp
-            rev_temp = temp
-            temp = next_node
+      while slow:
+            next_node = slow.next
+            slow.next = rev_temp
+            rev_temp = slow
+            slow = next_node
 
 
-        first = curr
+        first = head
         second = rev_temp
-        while second:  
+        while first and second:  
             tmp1 = first.next
             tmp2 = second.next
 
